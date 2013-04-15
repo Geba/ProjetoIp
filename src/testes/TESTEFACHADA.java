04e96452d78b3e13115cb3806231dc7c90af2a6b
Geovane Pereira <gsp@cin.ufpe.br>
2013-04-15 13:36:13 -0300

    testefachada

--- src/testes/TESTEFACHADA.java
+++ src/testes/TESTEFACHADA.java
@@ -2,6 +2,10 @@ package testes;
 
 import java.util.Iterator;
 
+import excecoes.ElementoJaCadastradoException;
+import excecoes.ElementoNaoEncontradoException;
+import excecoes.EntradaInvalidaException;
+import excecoes.RepositorioException;
 import fachadaEscola.Escola;
 import classesBase.*;
 import dados.*;
@@ -10,16 +14,25 @@ public class TESTEFACHADA {
 
 	/**
 	 * @param args
+	 * @throws RepositorioException 
+	 * @throws ElementoJaCadastradoException 
+	 * @throws EntradaInvalidaException 
+	 * @throws ElementoNaoEncontradoException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws EntradaInvalidaException, ElementoJaCadastradoException, RepositorioException, ElementoNaoEncontradoException {
 		Escola fachada = new Escola();
 
 		System.out.println("Pegando os alunoss: \n");
-		
+
 		Iterator<Pessoa> it1 = fachada.getAlunos().getIterator();
-		while(it1.hasNext()){
+		fachada.inserirTurma("Turma1");
+		
+		fachada.inserirAluno("000000000000134342", "heey", "01/01/1111", "8349638", "M",
+				"34626209", "rua", "1234", "bairro", "51030630", "cidade",
+				"estado", "pais", "pai", "mae",
+				fachada.getTurmas().procurar("Turma1"));
+		while (it1.hasNext()) {
 			System.out.println(it1.next().resumo());
 		}
 	}
-
 }
